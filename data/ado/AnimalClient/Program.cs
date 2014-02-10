47c9431ea82259791541a9d6f86e458f067452aa
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2014-02-10 18:39:16 +0100

    Added AnimalClient that uses AnimalEntities.

---
+++ data/ado/AnimalClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimalEntities;
+
+namespace AnimalClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new AnimalsContainer())
+            {
+                var nAnimals = ctx.AnimalSet.Count();
+                var nPersons = ctx.PersonSet.Count();
+
+                var dog = new Dog {PetName = "Lobi", RaceName = "Keeshond"};
+                ctx.AnimalSet.Add(dog);
+
+                var m = new Monkey {MonkeySpecial = "special string", RaceName = "Chimp"};
+                ctx.AnimalSet.Add(m);
+
+                ctx.SaveChanges();
+
+
+                RemoveAllAnimals(ctx);
+            }
+
+
+        }
+
+        private static void RemoveAllAnimals(AnimalsContainer ctx)
+        {
+            foreach (var animal in ctx.AnimalSet)
+            {
+                ctx.AnimalSet.Remove(animal);
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
