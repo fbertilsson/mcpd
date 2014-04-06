b1ffbaa3bc52d64fd97220809079a417b3185e70
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2014-04-06 22:34:42 +0200

    Made orders a detail view - responds to clicks in customer. Cose to show only a few columns.

--- data/ado/NorthwindPocoClient/App.xaml.cs
+++ data/ado/NorthwindPocoClient/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Configuration;
-using System.Windows;
+using System.Threading;
 using NorthwindPocoEntities;
 
 namespace NorthwindPocoClient
@@ -7,10 +7,13 @@ namespace NorthwindPocoClient
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
+            var c = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentUICulture = c; // but did not get swedish date format :-(
+
             var connectionString = ConfigurationManager.ConnectionStrings["NorthwindPocoEntities"].ConnectionString;
             var context = new NorthwindPocoContext(connectionString);
             var model = new MainWindowViewModel(context);
--- data/ado/NorthwindPocoClient/MainWindowViewModel.cs
+++ data/ado/NorthwindPocoClient/MainWindowViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using NorthwindPocoClient.PrismStuff;
 using NorthwindPocoEntities;
 
 namespace NorthwindPocoClient
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : NotificationObject
     {
         private readonly NorthwindPocoContext m_Context;
 
@@ -11,9 +15,42 @@ namespace NorthwindPocoClient
             get { return m_Context.Customers; }
         }
 
+        public IEnumerable<Order> CustomerOrders
+        {
+            get
+            {
+                IEnumerable<Order> result = null;
+                if (SelectedCustomer != null)
+                {
+                    result = from o in m_Context.Orders
+                        where o.CustomerID.Equals(SelectedCustomer.CustomerID)
+                        select o;
+                    //result = m_Context.Orders.Where(o => o.CustomerID == SelectedCustomer.CustomerID);
+                }
+                return result;
+            }
+        }
+
         public IEnumerable<Order> Orders
         {
-            get { return m_Context.Orders; }
+            get
+            {
+                return m_Context.Orders;
+            }
+        }
+
+        private Customer m_SelectedCustomer;
+
+        public Customer SelectedCustomer
+        {
+            get { return m_SelectedCustomer; }
+            set
+            {
+                if (value == m_SelectedCustomer)return;
+                m_SelectedCustomer = value;
+                RaisePropertyChanged(() => SelectedCustomer);
+                RaisePropertyChanged(() => CustomerOrders);
+            }
         }
 
         public MainWindowViewModel(NorthwindPocoContext context)
