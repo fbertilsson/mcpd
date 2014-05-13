b66c70c2479f5f3a2da0bdbba5046719f5c0363d
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2014-05-13 20:56:45 +0200

    Added first untested v of HierarchiesWihtEF4 with manual mappings.

---
+++ data/ado/HierarchiesWithEF4/Hierarchies.edmx.sql
@@ -0,0 +1,172 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/13/2014 20:39:44
+-- Generated from EDMX file: C:\arb\mcpd\data\ado\HierarchiesWithEF4\Hierarchies.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [hierarchies];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_TptMammal_inherits_TptAnimal]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TptAnimals_TptMammal] DROP CONSTRAINT [FK_TptMammal_inherits_TptAnimal];
+GO
+IF OBJECT_ID(N'[dbo].[FK_TptInsect_inherits_TptAnimal]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TptAnimals_TptInsect] DROP CONSTRAINT [FK_TptInsect_inherits_TptAnimal];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[TphAnimalSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[TphAnimalSet];
+GO
+IF OBJECT_ID(N'[dbo].[TptAnimals]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[TptAnimals];
+GO
+IF OBJECT_ID(N'[dbo].[TptAnimals_TptMammal]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[TptAnimals_TptMammal];
+GO
+IF OBJECT_ID(N'[dbo].[TptAnimals_TptInsect]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[TptAnimals_TptInsect];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'TphAnimalSet'
+CREATE TABLE [dbo].[TphAnimalSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'TptAnimals'
+CREATE TABLE [dbo].[TptAnimals] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'TphAnimalSet_TphMammal'
+CREATE TABLE [dbo].[TphAnimalSet_TphMammal] (
+    [DaysPregnancy] int  NOT NULL,
+    [Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'TphAnimalSet_TphInsect'
+CREATE TABLE [dbo].[TphAnimalSet_TphInsect] (
+    [AntennaCount] int  NOT NULL,
+    [Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'TptAnimals_TptMammal'
+CREATE TABLE [dbo].[TptAnimals_TptMammal] (
+    [DaysPregnancy] int  NOT NULL,
+    [Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'TptAnimals_TptInsect'
+CREATE TABLE [dbo].[TptAnimals_TptInsect] (
+    [AntennaCount] int  NOT NULL,
+    [Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'TphAnimalSet'
+ALTER TABLE [dbo].[TphAnimalSet]
+ADD CONSTRAINT [PK_TphAnimalSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TptAnimals'
+ALTER TABLE [dbo].[TptAnimals]
+ADD CONSTRAINT [PK_TptAnimals]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TphAnimalSet_TphMammal'
+ALTER TABLE [dbo].[TphAnimalSet_TphMammal]
+ADD CONSTRAINT [PK_TphAnimalSet_TphMammal]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TphAnimalSet_TphInsect'
+ALTER TABLE [dbo].[TphAnimalSet_TphInsect]
+ADD CONSTRAINT [PK_TphAnimalSet_TphInsect]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TptAnimals_TptMammal'
+ALTER TABLE [dbo].[TptAnimals_TptMammal]
+ADD CONSTRAINT [PK_TptAnimals_TptMammal]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TptAnimals_TptInsect'
+ALTER TABLE [dbo].[TptAnimals_TptInsect]
+ADD CONSTRAINT [PK_TptAnimals_TptInsect]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Id] in table 'TphAnimalSet_TphMammal'
+ALTER TABLE [dbo].[TphAnimalSet_TphMammal]
+ADD CONSTRAINT [FK_TphMammal_inherits_TphAnimal]
+    FOREIGN KEY ([Id])
+    REFERENCES [dbo].[TphAnimalSet]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Id] in table 'TphAnimalSet_TphInsect'
+ALTER TABLE [dbo].[TphAnimalSet_TphInsect]
+ADD CONSTRAINT [FK_TphInsect_inherits_TphAnimal]
+    FOREIGN KEY ([Id])
+    REFERENCES [dbo].[TphAnimalSet]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Id] in table 'TptAnimals_TptMammal'
+ALTER TABLE [dbo].[TptAnimals_TptMammal]
+ADD CONSTRAINT [FK_TptMammal_inherits_TptAnimal]
+    FOREIGN KEY ([Id])
+    REFERENCES [dbo].[TptAnimals]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Id] in table 'TptAnimals_TptInsect'
+ALTER TABLE [dbo].[TptAnimals_TptInsect]
+ADD CONSTRAINT [FK_TptInsect_inherits_TptAnimal]
+    FOREIGN KEY ([Id])
+    REFERENCES [dbo].[TptAnimals]
+        ([Id])
+    ON DELETE CASCADE ON UPDATE NO ACTION;
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
