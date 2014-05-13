f898fb4b5a588afb2b40b667f9feab734fa59701
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2014-05-13 21:44:00 +0200

    Working TPT and TPH example. Only add.

--- data/ado/HierarchiesWithEF4Client/Program.cs
+++ data/ado/HierarchiesWithEF4Client/Program.cs
@@ -18,24 +18,42 @@ namespace HierarchiesWithEF4Client
             using (var ctx = new HierarchiesContainer(connectionString))
             {
                 GoTph(ctx);
+                GoTpt(ctx);
                 ctx.SaveChanges();
             }
         }
 
         private void GoTph(HierarchiesContainer ctx)
         {
-            Console.Write("Add mammal, name? ");
+            Console.Write("Add TPH mammal, name? ");
             var name = Console.ReadLine();
-            if (name == null) return;
+            if (string.IsNullOrEmpty(name)) return;
             var mammal = new TphMammal {Name = name, DaysPregnancy = name.Length * 10};
             ctx.TphAnimalSet.AddObject(mammal);
 
 
-            Console.Write("Add insect, name? ");
+            Console.Write("Add TPH insect, name? ");
             name = Console.ReadLine();
-            if (name == null) return;
+            if (string.IsNullOrEmpty(name)) return;
             var insect = new TphInsect { Name = name, AntennaCount = name.Length };
             ctx.TphAnimalSet.AddObject(insect);
         }
+
+        private void GoTpt(HierarchiesContainer ctx)
+        {
+            Console.Write("Add TPT mammal, name? ");
+            var name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name)) return;
+            var mammal = new TptMammal { Name = name, DaysPregnancy = name.Length * 10 };
+            ctx.TptAnimals.AddObject(mammal);
+
+
+            Console.Write("Add TPT insect, name? ");
+            name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name)) return;
+            var insect = new TptInsect { Name = name, AntennaCount = name.Length };
+            ctx.TptAnimals.AddObject(insect);
+        }
+
     }
 }
