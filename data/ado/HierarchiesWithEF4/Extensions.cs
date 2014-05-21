70fb7eba4b12c01c57b98f82cc175454f0d7cf25
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2014-05-21 11:03:23 +0200

    Copy-to-output-directory example added (".\") in App.config Restructured into Read Eval Print

---
+++ data/ado/HierarchiesWithEF4/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace HierarchiesWithEF4
+{
+    public partial class HierarchiesContainer
+    {
+        // No, this can't be done:
+        // '...DeleteObject(object)': cannot override inherited member '...ObjectContext.DeleteObject(object)' because it is not marked virtual, abstract, or override
+        //public override void DeleteObject(object entity)
+        //{
+        //    base.DeleteObject(entity);
+        //}
+    }
+}
--- data/ado/HierarchiesWithEF4Client/Program.cs
+++ data/ado/HierarchiesWithEF4Client/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Configuration;
-using System.Data;
 using System.Data.Objects;
-using System.Data.SqlClient;
+using System.Data.Objects.DataClasses;
+using System.IO;
 using System.Linq;
 using HierarchiesWithEF4;
 
@@ -18,26 +18,72 @@ namespace HierarchiesWithEF4Client
 
         private void Go(string connectionString)
         {
-            try
+            bool doLoop = true;
+            while (doLoop)
             {
-                using (var ctx = new HierarchiesContainer(connectionString))
+                bool ok;
+                var chosenInt = Read(out ok);
+                if (! ok) continue;
+                if (chosenInt == 0) doLoop = false;
+                try
                 {
-                    GoTph(ctx);
-                    GoTpt(ctx);
-                    DetachModifyAttach(ctx);
-                    ctx.SaveChanges();
+                    var result = Eval(connectionString, chosenInt);
+                    Console.WriteLine(result.ToString());
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
         }
 
-        private void DetachModifyAttach(HierarchiesContainer ctx)
+        private static int Read(out bool ok)
+        {
+            Console.WriteLine("Choose:");
+            Console.WriteLine(" 0: Exit");
+            Console.WriteLine(" 1. New TPH");
+            Console.WriteLine(" 2. New TPT");
+            Console.WriteLine(" 3. Detach - modify - attach");
+            Console.WriteLine(" 4. Delete extension points TPH");
+            var choice = Console.ReadLine();
+            int chosenInt;
+            ok = int.TryParse(choice, out chosenInt);
+            return chosenInt;
+        }
+
+        private StringWriter Eval(string connectionString, int chosenInt)
+        {
+            StringWriter w = null;
+            using (var ctx = new HierarchiesContainer(connectionString))
+            {
+                switch (chosenInt)
+                {
+                    case 0:
+                        w = new StringWriter();
+                        break;
+                    case 1:
+                        w = GoTph(ctx);
+                        break;
+                    case 2:
+                        w = GoTpt(ctx);
+                        break;
+                    case 3:
+                        w = DetachModifyAttach(ctx);
+                        break;
+                    case 4:
+                        w = DeleteExtensionPoints(ctx);
+                        break;
+                }
+                ctx.SaveChanges();
+            }
+            return w;
+        }
+
+        private StringWriter DetachModifyAttach(HierarchiesContainer ctx)
         {
             var firstInsect = ctx.TphAnimalSet.OfType<TphInsect>().First();
-            Console.WriteLine("  Antenna count: {1}, State before detaching: {0}", firstInsect.EntityState, firstInsect.AntennaCount);
+            var w = new StringWriter();
+            w.WriteLine("  Antenna count: {1}, State before detaching: {0}", firstInsect.EntityState, firstInsect.AntennaCount);
             var copy = new TphInsect
             {
                 Id = firstInsect.Id,
@@ -46,15 +92,16 @@ namespace HierarchiesWithEF4Client
             };
             ctx.TphAnimalSet.Detach(firstInsect);
             firstInsect.AntennaCount += 1;
-            Console.WriteLine("  Detached and incremented antenna count. State now: {0}", firstInsect.EntityState);
+            w.WriteLine("  Detached and incremented antenna count. State now: {0}", firstInsect.EntityState);
             ctx.TphAnimalSet.Attach(firstInsect);
-            Console.WriteLine("  Attached. State now: {0}", firstInsect.EntityState);
+            w.WriteLine("  Attached. State now: {0}", firstInsect.EntityState);
             ctx.TphAnimalSet.ApplyOriginalValues(copy);
-            Console.WriteLine("  Applied original values. State now: {0}", firstInsect.EntityState);
+            w.WriteLine("  Applied original values. State now: {0}", firstInsect.EntityState);
             ctx.Refresh(RefreshMode.ClientWins, firstInsect);
-            Console.WriteLine("  Refreshed. State now: {0}", firstInsect.EntityState);
+            w.WriteLine("  Refreshed. State now: {0}", firstInsect.EntityState);
             ctx.SaveChanges();
-            Console.WriteLine("  Saved with no options. State now: {0}", firstInsect.EntityState);
+            w.WriteLine("  Saved with no options. State now: {0}", firstInsect.EntityState);
+            return w;
         }
 
         private void DetachModifyAttachTake1(HierarchiesContainer ctx)
@@ -74,37 +121,62 @@ namespace HierarchiesWithEF4Client
             Console.WriteLine("  Saved with no options. State now: {0}", firstInsect.EntityState);
         }
         
-        private void GoTph(HierarchiesContainer ctx)
+        private StringWriter GoTph(HierarchiesContainer ctx)
         {
+            var w = new StringWriter();
             Console.Write("Add TPH mammal, name? ");
             var name = Console.ReadLine();
-            if (string.IsNullOrEmpty(name)) return;
+            if (string.IsNullOrEmpty(name)) return w;
             var mammal = new TphMammal {Name = name, DaysPregnancy = name.Length * 10};
             ctx.TphAnimalSet.AddObject(mammal);
 
 
             Console.Write("Add TPH insect, name? ");
             name = Console.ReadLine();
-            if (string.IsNullOrEmpty(name)) return;
+            if (string.IsNullOrEmpty(name)) return w;
             var insect = new TphInsect { Name = name, AntennaCount = name.Length };
             ctx.TphAnimalSet.AddObject(insect);
+            return w;
         }
 
-        private void GoTpt(HierarchiesContainer ctx)
+        private StringWriter GoTpt(HierarchiesContainer ctx)
         {
+            var w = new StringWriter();
             Console.Write("Add TPT mammal, name? ");
             var name = Console.ReadLine();
-            if (string.IsNullOrEmpty(name)) return;
+            if (string.IsNullOrEmpty(name)) return w;
             var mammal = new TptMammal { Name = name, DaysPregnancy = name.Length * 10 };
             ctx.TptAnimals.AddObject(mammal);
 
 
             Console.Write("Add TPT insect, name? ");
             name = Console.ReadLine();
-            if (string.IsNullOrEmpty(name)) return;
+            if (string.IsNullOrEmpty(name)) return w;
             var insect = new TptInsect { Name = name, AntennaCount = name.Length };
             ctx.TptAnimals.AddObject(insect);
+            return w;
         }
+
+        private StringWriter DeleteExtensionPoints(HierarchiesContainer ctx)
+        {
+            var w = new StringWriter();
+            Console.Write("Enter id to delete: ");
+            var idString = Console.ReadLine();
+            var id = int.Parse(idString);
+            var animal = ctx.TphAnimalSet.FirstOrDefault(x => x.Id == id);
+            if (animal == null)
+            {
+                w.WriteLine("Could not find animal with id {0}", idString);
+            }
+            else
+            {
+                ctx.DeleteObject(animal);               // either
+                ctx.TphAnimalSet.DeleteObject(animal);  // or, but found no extension point in EF. Only in Linq2Sql???
+                w.WriteLine("Deleted.");
+            }
+            return w;
+        }
+
 
     }
 }
